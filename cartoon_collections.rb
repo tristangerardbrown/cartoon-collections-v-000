bcb1bf6c59eeff23d7598a624bf48b34245b7a39
Tristan Brown <tgbrown@stanford.edu>
2018-08-23 06:15:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+    names.each_with_index do |name, index|
+  puts "#{index+1} #{name}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |planeteer| planeteer.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
 end
 
 def find_the_cheese# code an argument here
